d6d28ecf38d5a687ae8d89b7e08dd23fe3b91f1d
HarishIsCool <harish.skt4@gmail.com>
2025-01-18 14:48:46 -0500

    Add BME code temp sd code

--- arduino_workshop_harish/arduino_workshop_harish.ino
+++ arduino_workshop_harish/arduino_workshop_harish.ino
@@ -1,9 +1,88 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <TinyBME280.h>
 
+void setup() {
+  // Runs once at start or board reset
+
+  // Uncomment to change address. Default is 0x77
+  //BME280setI2Caddress(address)
+
+  // Begin the I2C connection with BME280
+  BME280setup();
+
+  // Begin Serial monitor output for debugging
+  Serial.begin(9600);
+
+  Serial.println("BME280 Interface Example.");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Runs forever after setup
 
+  // Gives the temperature as a signed 32-bit integer in °C with a resolution of 0.01°C. So an output value of “5123” equals 51.23°C.
+  float temp = BME280temperature() / 100;
+
+  // Pives the pressure in Pa as an unsigned 32-bit integer, so an output value of “96386” equals 96386 Pa, or 963.86 hPa.
+  float press = BME280pressure() / 100;
+
+  // Gives the humidity in %RH with a resolution of 0.01% RH, so an output value of “4653” represents 46.53 %RH.
+  float humid = BME280humidity() / 100;
+
+
+  // Print the data to Serial output!
+  Serial.print("Temperature: ");
+  Serial.print(temp);
+
+  Serial.print(", Pressure: ");
+  Serial.print(press);
+
+  Serial.print(", Humidity: ");
+  Serial.println(humid);
+
+  delay(1000);
 }
+
+
+#include <SPI.h>
+#include <SD.h>
+
+const int chipSelect = 4;
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  // wait for Serial Monitor to connect. Needed for native USB port boards only:
+  while (!Serial);
+
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin(chipSelect)) {
+    Serial.println("you failed bozo. Things to check:");
+    Serial.println("1. did you forget to put the card in again");
+    Serial.println("2. did you mess up the wiring like everything you do?");
+    Serial.println("3. did you change the chipSelect pin to match your shield or module?");
+    Serial.println("did your dementia kick in and you forgot somthing again");
+    Serial.println("Note: press reset button on the board and reopen this Serial Monitor after fixing your issue!");
+    while (true);
+  }
+
+  Serial.println("initialization done.");
+}
+
+void loop() {
+  
+  String dataString = "Harish";
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+
+  // if the file is available, write to it:
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();
+    // print to the serial port too:
+    Serial.println(dataString);
+  }
+  // if the file isn't open, pop up an error:
+  else {
+    Serial.println("error opening datalog.txt");
+  }
+}
+
